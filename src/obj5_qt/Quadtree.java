4daf95795bf2b32b2be69f59730631507cfc4b49
Gabriel Oliveira <77310038+GS-Moonlyn@users.noreply.github.com>
2022-02-01 18:08:08 -0300

    Branch de Quadtree

---
+++ src/obj5_qt/Quadtree.java
@@ -0,0 +1,71 @@
+package obj5_qt;
+
+import java.util.ArrayList;
+
+public class Quadtree {
+
+	private Boundary boundary;
+	private int depth;
+	private int capacity;
+	private ArrayList<Point> points = new ArrayList<Point>();
+	private boolean isDivided = false;
+	Quadtree northeast;
+	Quadtree northwest;
+	Quadtree southeast;
+	Quadtree southwest;
+	
+	public Quadtree(Boundary boundary, int capacity, int depth) {	
+		this.boundary = boundary;
+		this.capacity = capacity;
+		this.depth = depth;
+	}
+	
+	public void subdivide() {
+		Boundary nw = new Boundary(boundary.getX() - boundary.getWidth() / 2, 
+									 boundary.getY() - boundary.getHeight() / 2,
+									 boundary.getWidth() / 2, boundary.getHeight() / 2);
+		northwest = new Quadtree(nw, capacity, depth - 1);
+	
+		Boundary ne = new Boundary(boundary.getX() + boundary.getWidth() / 2, 
+									 boundary.getY() - boundary.getHeight() / 2,
+									 boundary.getWidth() / 2, boundary.getHeight() / 2);
+		northeast = new Quadtree(ne, capacity, depth - 1);
+		
+		Boundary sw = new Boundary(boundary.getX() - boundary.getWidth() / 2, 
+				 					 boundary.getY() + boundary.getHeight() / 2,
+				 					 boundary.getWidth() / 2, boundary.getHeight() / 2);
+		southwest = new Quadtree(sw, capacity, depth - 1);
+		
+		Boundary se = new Boundary(boundary.getX() + boundary.getWidth() / 2, 
+				 					 boundary.getY() + boundary.getHeight() / 2,
+				 					 boundary.getWidth() / 2, boundary.getHeight() / 2);
+		southeast = new Quadtree(se, capacity, depth - 1);
+		isDivided = true;
+	}	
+	
+	public void insert(Point pnt) {
+		
+		if(!this.boundary.contains(pnt)) {
+			return;
+		} 
+		if(this.points.size() < capacity) {
+			this.points.add(pnt);
+		}
+		else {
+			if(!isDivided && depth > 0) {
+				this.subdivide();
+				System.out.println("Entrando na profundidade: " + depth);
+				this.points.add(pnt);
+				for (Point point : points) {
+					this.northeast.insert(point);
+					this.northwest.insert(point);
+					this.southeast.insert(point);
+					this.southwest.insert(point);
+				}
+				this.points.clear();
+			} else {
+				this.points.add(pnt);
+			}
+		}
+	}
+}
